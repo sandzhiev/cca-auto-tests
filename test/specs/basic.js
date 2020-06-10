77c08614eb865790273a4831105bd2b96b1337b6
NamruSanjin <n.sandzhiev@gmail.com>
2020-06-10 11:02:27 -0400

    1.1

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,24 +1,44 @@
+
+const URL = 'https://likejean.github.io/homework-5/';
+
 describe('Test RFL', () => {
-    it('should Verify that RFL accepts 1', () => {
-        browser.url('https://likejean.github.io/homework-5/');
-        const rfl = $('//button[@name="positive"]');
-        rfl.click();
-        const rflValue = $('//input[@name="upper"]');
-        rflValue.setValue('1');
-        const buttonActual = $('//button[@step="1"]').getText();
-        expect(buttonActual).toEqual('1');
+
+    before('App URL', () => {
+        browser.url(URL);
+    });
+
+    it('Verify that the header "Counters" is present', () => {
+        const actualHeader = $('h1').getText();
+        const expected = 'Counters';
+        expect(actualHeader).toEqual(expected);
     });
 
 
-    it('should Verify that RFL accepts 9', () => {
-        browser.url('https://likejean.github.io/homework-5/');
-        const rfl = $('//button[@name="positive"]');
-        rfl.click();
-        const rflValue = $('//input[@name="upper"]');
-        rflValue.setValue('9');
-        const buttonActual = $('//button[@step="9"]').getText();
-        expect(buttonActual).toEqual('9');
-    });
+
+
+
+
+
+    // it('should Verify that RFL accepts 1', () => {
+    //     browser.url('https://likejean.github.io/homework-5/');
+    //     const rfl = $('//button[@name="positive"]');
+    //     rfl.click();
+    //     const rflValue = $('//input[@name="upper"]');
+    //     rflValue.setValue('1');
+    //     const buttonActual = $('//button[@step="1"]').getText();
+    //     expect(buttonActual).toEqual('1');
+    // });
+    //
+    //
+    // it('should Verify that RFL accepts 9', () => {
+    //     browser.url('https://likejean.github.io/homework-5/');
+    //     const rfl = $('//button[@name="positive"]');
+    //     rfl.click();
+    //     const rflValue = $('//input[@name="upper"]');
+    //     rflValue.setValue('9');
+    //     const buttonActual = $('//button[@step="9"]').getText();
+    //     expect(buttonActual).toEqual('9');
+    // });
 
     // it('should Verify that RFL does not accept "e" ', () => {
     //     browser.url('https://likejean.github.io/homework-5/');
