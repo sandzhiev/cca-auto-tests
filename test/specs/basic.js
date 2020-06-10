7fac8a2f3102c0856cbd27a1d481e56157aaa95e
NamruSanjin <n.sandzhiev@gmail.com>
2020-06-10 00:50:39 -0400

    1.1

---
+++ test/specs/basic.js
@@ -0,0 +1,42 @@
+describe('Test RFL', () => {
+    it('should Verify that RFL accepts 1', () => {
+        browser.url('https://likejean.github.io/homework-5/');
+        const rfl = $('//button[@name="positive"]');
+        rfl.click();
+        const rflValue = $('//input[@name="upper"]');
+        rflValue.setValue('1');
+        const buttonActual = $('//button[@step="1"]').getText();
+        expect(buttonActual).toEqual('1');
+    });
+
+
+    it('should Verify that RFL accepts 9', () => {
+        browser.url('https://likejean.github.io/homework-5/');
+        const rfl = $('//button[@name="positive"]');
+        rfl.click();
+        const rflValue = $('//input[@name="upper"]');
+        rflValue.setValue('9');
+        const buttonActual = $('//button[@step="9"]').getText();
+        expect(buttonActual).toEqual('9');
+    });
+
+    // it('should Verify that RFL does not accept "e" ', () => {
+    //     browser.url('https://likejean.github.io/homework-5/');
+    //     const rfl = $('//button[@name="positive"]');
+    //     rfl.click();
+    //     const rflValue = $('//input[@name="upper"]');
+    //     rflValue.setValue('e');
+    //     const alertActual = $('//div[@role="alert"]//span').getValue();
+    //     expect(alertActual).toEqual("ERROR: Input must be an INTEGER")
+    // });
+
+    // it('should Verify that RFL does not accept "$" ', () => {
+    //     browser.url('https://likejean.github.io/homework-5/');
+    //     const rfl = $('//button[@name="positive"]');
+    //     rfl.click();
+    //     const rflValue = $('//input[@name="upper"]');
+    //     rflValue.setValue('$');
+    //     const alertActual = $('//div[@role="alert"]//span').getText();
+    //     expect(alertActual).toEqual("ERROR: Input must be an INTEGER")
+    // });
+});
